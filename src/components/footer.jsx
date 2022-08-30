0e50a5e5a26e3b8638aa16e011b1168c93568db1
Roweens <enderman2011@gmail.com>
2022-08-30 23:17:51 +0300

    gh-pages added

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -4,7 +4,10 @@ export function Footer() {
       <div className="footer-copyright">
         <div className="container">
           © {new Date().getFullYear()} Copyright Text
-          <a className="grey-text text-lighten-4 right" href="#!">
+          <a
+            className="grey-text text-lighten-4 right"
+            href="https://github.com/Roweens/react-shop"
+          >
             Repo
           </a>
         </div>
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,12 +2,12 @@ export function Header() {
   return (
     <nav className="deep-purple darken-3">
       <div className="nav-wrapper">
-        <a href="#" className="brand-logo">
+        <a href="!#" className="brand-logo">
           React Shop
         </a>
         <ul id="nav-mobile" className="right hide-on-med-and-down">
           <li>
-            <a href="#">Repo</a>
+            <a href="https://github.com/Roweens/react-shop">Repo</a>
           </li>
         </ul>
       </div>
